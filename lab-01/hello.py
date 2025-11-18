353b09bb3f1beb1995de227ecb00acb2d3ff9ed4
thnhphong <thnhphong4869@gmail.com>
2025-11-18 08:39:53 +0700

    lab01 hello.py

---
+++ lab-01/hello.py
@@ -0,0 +1,5 @@
+print("Hello world");
+print("My name is To Thanh Phong");
+print("Student ID: 2280602381");
+print("I am studying at HUTECH University");
+print("I am learning Python programming language");
